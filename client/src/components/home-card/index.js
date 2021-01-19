38d0217b8e3382301462695d94ae15e7c3b10fae
JonathanRaso <jonathanraso.dev@gmail.com>
2021-01-19 17:41:26 +0100

    Overlay effect on home cards added (need to change colors and font later). Need to fix flexbox display for home cards (cards don't shrink/grow while changing screen size)

--- client/src/components/home-card/index.js
+++ client/src/components/home-card/index.js
@@ -4,12 +4,17 @@ import './styles.css';
 
 const HomeCard = ({id, title, description, imageUrl}) => {
   return (
-    <Link to={`/${id}`} className="card__test">
-      <div key={`${id}`}>
-        <h3>{`${id}`}</h3>
-        <h3>{`${title}`}</h3>
-        <h3>{`${description}`}</h3>
-        <h3>{`${imageUrl}`}</h3>
+    <Link to={`/${id}`} className="">
+      <div key={`${id}`} className="card__body">
+        <img 
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.J0JE-fKbFT4bxpp8ilPpEQHaHa%26pid%3DApi&f=1" 
+          alt=""
+          className="card__body__image"
+        />
+        <div className="card__overlay card__overlay--primary">
+          <h3 className="card__overlay__title">{`${title}`}</h3>
+          <p className="card__overlay__description">{`${description}`}</p>
+        </div>
       </div>
     </Link>
   )

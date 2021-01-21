f2a055ffcb594c23ba952d326ea3968ad61a9a97
JonathanRaso <jonathanraso.dev@gmail.com>
2021-01-21 15:24:07 +0100

    Flexbox display for homepage fixed. Ok for the moment, need to add media queries later

--- client/src/components/home-card/index.js
+++ client/src/components/home-card/index.js
@@ -4,37 +4,22 @@ import './styles.css';
 
 const HomeCard = ({id, title, description, imageUrl}) => {
 
-  /* const overlayColorPicker = () => {
-    const colorClassName = ['primary', 'secondary', 'thirdly'];
-
-    let color;
-    colorClassName.map((className) => {
-      color = colorClassName[0];
-      colorClassName.shift();
-      console.log(color);
-      console.log(colorClassName.length);
-      if (colorClassName.length === 1) {
-        colorClassName.push('primary', 'secondary');
-      }
-      return color;
-    });
-  };
-  let colorClass = overlayColorPicker(); */
-
   return (
-    <Link to={`/${id}`} className="card">
-      <div key={`${id}`} className="card__body">
-        <img 
-          src="https://unsplash.it/450/450" 
-          alt=""
-          className="card__body__image"
-        />
-        <div className={`card__overlay card__overlay--primary`}>
-          <h3 className="card__overlay__title">{`${title}`}</h3>
-          <p className="card__overlay__description">{`${description}`}</p>
+    <div className="card__container">
+      <Link to={`/${id}`}>
+        <div key={`${id}`} className="card__body">
+          <img 
+            src="https://source.unsplash.com/random" 
+            alt=""
+            className="card__body__image"
+          />
+          <div className="card__overlay">
+            <h3 className="card__overlay__title">{`${title}`}</h3>
+            <p className="card__overlay__description">{`${description}`}</p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
 

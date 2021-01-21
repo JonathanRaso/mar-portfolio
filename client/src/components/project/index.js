319f46a7354739b89d83192fda5dcf34743712b3
JonathanRaso <jonathanraso.dev@gmail.com>
2021-01-21 17:49:01 +0100

    Style for project page started, need to check mobile and finish desktop version

--- client/src/components/project/index.js
+++ client/src/components/project/index.js
@@ -9,12 +9,22 @@ const ProjectDetails = () => {
   let { projectId } = useParams();
 
   const [specificProject, setSpecificProject] = useState();
+  const [backgroundColor, setBackgroundColor] = useState();
+
+  useEffect(() => {
+    const randomColors = ['first', 'second', 'third', 'fourth'];
+    const backgroundInfoColor = (colorsArray) => {
+      setBackgroundColor(colorsArray[Math.floor(Math.random() * colorsArray.length)]);
+    };
+    backgroundInfoColor(randomColors);
+    console.log('result: ' + backgroundColor);
+  }, [backgroundColor]);
 
   useEffect(() => {
     const fetchSpecificProject = async () => {
       try {
         const projectData = await axios.get(`http://localhost:5000/api/projects/${projectId}`);
-
+        
         setSpecificProject(projectData.data.project);
       } catch (err) {
         console.log(err);
@@ -25,15 +35,21 @@ const ProjectDetails = () => {
 
   return (
     <main className="main__height project__body">
-      <h2> Project id = { projectId } </h2>
-      {specificProject &&
-      <>
-        <h3>{specificProject.id}</h3>
-        <h3>{specificProject.title}</h3>
-        <h3>{specificProject.description}</h3>
-        <h3>{specificProject.imageUrl}</h3>
-      </> 
-      }
+      <div className="project__picture">
+        {specificProject &&
+        <>
+          <img src="https://source.unsplash.com/random" alt=""/>
+        </> 
+        }
+      </div>
+      <div className={`project__info project__info--${backgroundColor}`}>
+        {specificProject &&
+        <>
+          <h2>{specificProject.title}</h2>
+          <p>{specificProject.description}</p>
+        </> 
+        }
+      </div>
     </main>
   )
 };

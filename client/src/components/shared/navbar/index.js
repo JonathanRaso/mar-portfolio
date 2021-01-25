73988e65f3b1ce9fc0d9902926970d5923a23726
JonathanRaso <jonathanraso.dev@gmail.com>
2021-01-25 18:07:51 +0100

    Mobile menu started, onClick handler added with classList.toggle(). Need to finish it later and find a way to display menu only when .open is added

--- client/src/components/shared/navbar/index.js
+++ client/src/components/shared/navbar/index.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import '../../../App.css';
 import './styles.css';
 
 const Navbar = () => {
+
+  const [openMenu, setOpenMenu] = useState(false);
+
+
+  const handleMenuButton = () => {
+    const burgerMenu = document.querySelector(".navbar__hamburger");
+    /* console.log(burgerMenu);
+    setOpenMenu(!openMenu);
+    console.log(openMenu);
+    return burgerMenu; */
+    console.log(burgerMenu.classList);
+    burgerMenu.classList.toggle("open");
+    console.log(burgerMenu.classList);
+  }
+
   return (
     <div className="container">
       <nav className="navbar__body">
+        <button className={`navbar__hamburger`} onClick={handleMenuButton}>
+          <img src="/MAR-portfolio-menu.png" alt=""/> 
+        </button>
         <ul className="navbar__ul flex">
           <li>
             <NavLink to="/" exact className="navbar__ul--link">Réalisations</NavLink>

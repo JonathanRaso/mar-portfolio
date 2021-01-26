319c2c4cd250c35ab9301fa99c9b4e02ca6201d1
JonathanRaso <jonathanraso.dev@gmail.com>
2021-01-26 18:05:44 +0100

    Infos page style started, mobile style almost finished. Need to to tablet and desktop style tomorrow.

--- client/src/components/infos/index.js
+++ client/src/components/infos/index.js
@@ -3,8 +3,29 @@ import './styles.css'
 
 const Infos = () => {
   return (
-    <main className="main__height infos__body">
-      <h2>Infos page</h2>
+    <main className="main__height">
+      <div className="infos__container">
+        <h2 className="infos__title">Bonjour!</h2>
+        <img className="infos__avatar" src="/MAR-portfolio-portrait.jpg" alt="portrait de l'artiste"/>
+        <div className="infos__description">
+          <h3 className="infos__description--title">Description</h3>
+          <p className="infos__description--text">
+            ipsum dolor sit amet, consectetur adipisicing elit. Mollitia aliquid provident unde illum, magnam reiciendis eligendi aliquam commodi sint quidem. Omnis repellat ducimus exercitationem esse!
+          </p>
+        </div>
+        <div className="infos__medium">
+          <h3 className="infos__medium--title">TECHNIQUES</h3>
+          <p className="infos__medium--text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia aliquid provident unde illum, magnam reiciendis eligendi aliquam commodi sint quidem. Omnis repellat ducimus exercitationem esse!
+          </p> 
+        </div>
+        <div className="infos__social">
+          <h3 className="infos__social--title">Socials</h3>
+            <span>Email</span>
+            <span>Facebook</span>
+            <span>Pinterest</span>
+        </div>
+      </div>
     </main>
   )
 };

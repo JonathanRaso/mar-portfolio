4aa3f6a7a39aedea5ebfeb3872a121ca1ef53dd1
JonathanRaso <jonathanraso.dev@gmail.com>
2021-01-28 17:58:25 +0100

    Deleting project button added when admin is logged. Project is deleted and admin redirect to homepage after it. Create new project form added, need to handle this and also create the edit form tomorrow

--- client/src/components/admin/admin-dashboard/index.js
+++ client/src/components/admin/admin-dashboard/index.js
@@ -13,12 +13,16 @@ const AdminDashboard = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmitForm = (event) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  
+  const handleSubmitLoginForm = (event) => {
     event.preventDefault();
-    const form = {username, password};
-    console.log(form);
-    console.log(login);
-    console.log("Form submitted");
+    const loginForm = {username, password};
+    /* console.log(username, password); */
+    console.log(loginForm);
+    console.log("loginForm submitted");
       
     axios.post("http://localhost:5000/api/users/login", { username, password })
     .then(function (response) {
@@ -34,23 +38,46 @@ const AdminDashboard = () => {
     });  
   }
 
+  const handleSubmitCreateForm = (event) => {
+    event.preventDefault();
+    const createForm = { title, description, imageUrl };
+    console.log(createForm);
+  }
+
   return (
-    <main>
-      <div className="admin__container">
-        <form onSubmit={handleSubmitForm} action="" method="POST">
+    <main> 
+      {!login &&
+        <div className="admin__container">
+          <form onSubmit={handleSubmitLoginForm} action="" method="POST">
 
-          <label htmlFor="username">Nom d'utilisateur</label>
-          <input id="username" type="text" name="username" value={username} onChange={(event) => {setUsername(event.target.value); console.log('username is : ' + username)}} required/>
+            <label htmlFor="username">Nom d'utilisateur</label>
+            <input id="username" type="text" name="username" value={username} onChange={(event) => {setUsername(event.target.value); console.log('username is : ' + username)}} required/>
 
-          <label htmlFor="password">Mot de passe</label>
-          <input id="password" type="password" name="password" value={password} onChange={(event) => {setPassword(event.target.value); console.log('password is : ' + password)}} required/>
+            <label htmlFor="password">Mot de passe</label>
+            <input id="password" type="password" name="password" value={password} onChange={(event) => {setPassword(event.target.value); console.log('password is : ' + password)}} required/>
 
-          <input type="submit" value="Connexion"/>
-        </form>
-        <p>{login}</p>
-        {login && <p>Tu es connecté</p>}
-        <button onClick={() => {setLogin('log'); console.log(login)}}>Change Value</button>
-      </div>
+            <input type="submit" value="Connexion"/>
+          </form>
+        </div>
+      }
+      
+      {login && 
+        <div className="admin__container">        
+          <form onSubmit={handleSubmitCreateForm} action="" method="POST">
+
+          <label htmlFor="title">Titre du projet</label>
+          <input id="title" type="text" name="title" value={title} onChange={(event) => {setTitle(event.target.value); console.log('title is : ' + title)}}required/>
+
+          <label htmlFor="description">Description du projet</label>
+          <input id="description" type="textarea" name="description" value={description} onChange={(event) => {setDescription(event.target.value); console.log('description is : ' + description)}} required/>
+
+          <label htmlFor="imageUrl">Description du projet</label>
+          <input id="imageUrl" type="file" name="imageUrl" value={imageUrl} onChange={(event) => {setImageUrl(event.target.value); console.log('imageUrl is : ' + imageUrl)}} required/>
+
+          <input type="submit" value="Valider"/>
+          </form>
+        </div>  
+      }
     </main>
   )
 };
--- client/src/components/project/index.js
+++ client/src/components/project/index.js
@@ -1,15 +1,31 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
+
+import { AuthContext } from '../context/auth-context';
 
 import '../../App.css';
 import './styles.css';
 
 const ProjectDetails = () => {
   let { projectId } = useParams();
+  let history = useHistory();
+
+  const { login } = useContext(AuthContext);
 
   const [specificProject, setSpecificProject] = useState();
   const [backgroundColor, setBackgroundColor] = useState();
+
+  const handleDeleteButton = async () => {
+    /* console.log("suppression du projet numéro" + projectId); */
+    try {
+      await axios.delete(`http://localhost:5000/api/projects/${projectId}`);
+      console.log("Projet supprimé!");
+    } catch (error) {
+      console.log(error);
+    }
+    return history.push("/");
+  }
 
   useEffect(() => {
     const randomColors = ['first', 'second', 'third', 'fourth'];
@@ -35,6 +51,13 @@ const ProjectDetails = () => {
 
   return (
     <main className="project__body">
+      {login && 
+        <div>
+          <button onClick={handleDeleteButton}>Supprimer ce projet</button>
+          <button onClick={() => console.log('Projet modifié')}>Valider changement</button>
+        </div>
+      }
+
       <div className={`project__picture project__picture--${backgroundColor}`}>
         {specificProject &&
         <>
